f5b9cf4d7757b120c3cdbd057565599c79f74734
teroqim <teroqim@gmail.com>
2017-02-18 19:40:55 +0100

    scroll

--- src/index.js
+++ src/index.js
@@ -200,4 +200,61 @@ module.exports = {
     return `${minutes} min left`
   },
   usernameDescription: 'Your username can only include letters, numbers and hyphens. It must also contain 3 or more characters',
+  scrollToY: (scrollTargetY, speed, easing) => {
+    // http://stackoverflow.com/questions/8917921/cross-browser-javascript-not-jquery-scroll-to-top-animation
+
+    // first add raf shim
+    // http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+    const requestAnimFrame = (() => {
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              (callback => {
+                window.setTimeout(callback, 1000 / 60)
+              })
+    })()
+    // scrollTargetY: the target scrollY property of the window
+    // speed: time in pixels per second
+    // easing: easing equation to use
+    const scrollY = window.scrollY || document.documentElement.scrollTop
+    scrollTargetY = scrollTargetY || 0
+    speed = speed || 2000
+    easing = easing || 'easeOutSine'
+    var currentTime = 0
+
+    // min time .1, max time .8 seconds
+    const time = Math.max(.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, .8))
+
+    // easing equations from https://github.com/danro/easing-js/blob/master/easing.js
+    const easingEquations = {
+      easeOutSine: pos => Math.sin(pos * (Math.PI / 2)),
+      easeInOutSine: pos => (-0.5 * (Math.cos(Math.PI * pos) - 1)),
+      easeInOutQuint: pos => {
+        if ((pos /= 0.5) < 1) {
+            return 0.5 * Math.pow(pos, 5)
+        }
+        return 0.5 * (Math.pow((pos - 2), 5) + 2)
+      }
+    }
+
+    // add animation loop
+    const tick = () => {
+      currentTime += 1 / 60
+
+      const p = currentTime / time
+      const t = easingEquations[easing](p)
+
+      if (p < 1) {
+        requestAnimFrame(tick)
+        window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t))
+      }
+      else {
+        // console.log('scroll done')
+        window.scrollTo(0, scrollTargetY)
+      }
+    }
+
+    // call it once to get started
+    tick()
+  },
 }
